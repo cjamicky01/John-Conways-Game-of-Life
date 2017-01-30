73d947cac33a0a14cfccf2feee1cb1d6fa1b8514
Connor <cjamicky02@gmail.com>
2017-01-30 14:29:05 -0500

    Commented Code

--- project01.cpp
+++ project01.cpp
@@ -30,7 +30,7 @@ void LoadGrid(ifstream& inFile, int grid[][CMAX]) {
 	getline(inFile, line);
 		for (int g = 0; g < CMAX; g++) {
 		for (int h = 0; h < RMAX; h++) {
-			getline(inFile, line, ' ');
+			getline(inFile, line, ' '); //Pull until space is hit
 			grid[g][h] = atoi(line.c_str()); //pull values of grid in text file here
 		}
 	}
@@ -40,22 +40,22 @@ void ComputeNextGrid(int current[][CMAX], int next[][CMAX], int birth[], int sur
 	int count;
 	for (int i = 0; i < CMAX; i++) {
 		for (int j = 0; j < RMAX; j++) {
-			int neigh1 = CountType1Neighbors(current, i, j);
+			int neigh1 = CountType1Neighbors(current, i, j); //Get neighbors for cell
 			int neigh2 = CountType2Neighbors(current, i, j);
 
-			if (current[i][j] == 1) {
-				if (survival[neigh1] == 1) next[i][j] = 1;
+			if (current[i][j] == 1) {						//Check what current cell value is
+				if (survival[neigh1] == 1) next[i][j] = 1;	//Check if matches any survival rule
 				else {
-					next[i][j] = 0;					
+					next[i][j] = 0;							//Otherwise cell Dies
 				}
 			}
-			else if (current[i][j] == 2) {
+			else if (current[i][j] == 2) {					//Repeat if Cell is 2
 				if (survival[neigh2] == 1) next[i][j] = 2;
 				else {
 					next[i][j] = 0;
 				}
 			}
-			else if (neigh1 > neigh2) {
+			else if (neigh1 > neigh2) {						//Cell birth same process
 				if (birth[neigh1] == 1) next[i][j] = 1;
 				else {
 					next[i][j] = 0;
@@ -67,7 +67,7 @@ void ComputeNextGrid(int current[][CMAX], int next[][CMAX], int birth[], int sur
 					next[i][j] = 0;
 				}
 			}
-			else {
+			else {											//Otherwise set zero
 				next[i][j] = 0;
 			}
 
@@ -79,7 +79,7 @@ void ComputeNextGrid(int current[][CMAX], int next[][CMAX], int birth[], int sur
 void CopyGrid(const int source[][CMAX], int destination[][CMAX]) {
 	for (int i = 0; i < CMAX; i++) {
 		for (int j = 0; j < RMAX; j++) {
-			destination[i][j] = source[i][j];
+			destination[i][j] = source[i][j];		//Loop through and copy each cell to corresponding cell
 		}
 	}
 
@@ -89,10 +89,10 @@ int CountType2Neighbors(int grid[][CMAX], int row, int col) {
 	int Total2Neighbors = 0;
 	for (int ir = -1; ir <= 1; ir++) {
 		for (int ic = -1; ic <= 1; ic++) {
-			if (grid[(row + ir + RMAX) % RMAX][(col + ic + CMAX) % CMAX] == 2) {
-				if(((row+ir+RMAX)%RMAX) == row && ((col+ic+CMAX)%CMAX) == col){
-				} else {
-				Total2Neighbors++;
+			if (grid[(row + ir + RMAX) % RMAX][(col + ic + CMAX) % CMAX] == 2) {	//Loop through each cell wrapping on the edges
+				if(((row+ir+RMAX)%RMAX) == row && ((col+ic+CMAX)%CMAX) == col){		//Check that its not the center cell
+				} else {															// row + ir + RMAX % RMAX give the surrounding cells and wraps because of the + RMAX % RMAX
+				Total2Neighbors++;													//Increase neighbor count otherwise
 				}
 			}
 		}
@@ -101,7 +101,7 @@ int CountType2Neighbors(int grid[][CMAX], int row, int col) {
 }
 
 int CountType1Neighbors(int grid[][CMAX], int row, int col) {
-	int Total1Neighbors = 0;
+	int Total1Neighbors = 0;														//Same process as for cell 2 neighbors
 	for (int ir = -1; ir <= 1; ir++) {
 		for (int ic = -1; ic <= 1; ic++) {
 			if (grid[(row + ir + RMAX) % RMAX][(col + ic + CMAX) % CMAX] == 1) {
@@ -116,11 +116,11 @@ int CountType1Neighbors(int grid[][CMAX], int row, int col) {
 }
 
 void ParseRequirementsString(string requirements, int reqs[]) {
-	const char *buffer = requirements.c_str();
-	char c;
-	while (c = *buffer++)
+	const char *buffer = requirements.c_str();										//Set the string to a char array
+	char c;		
+	while (c = *buffer++)															//Loop through until null
 	{
-		if (c == '0') {
+		if (c == '0') {																//Check for each possible value between 0 and 9 and assign it to corresponding location
 			reqs[0] = 0;
 		}
 		if (c == '1') {
