12a82f7cd600d404947232f89b01781e11b90a63
Connor <cjamicky02@gmail.com>
2017-01-30 14:17:49 -0500

    New compute

--- project01.cpp
+++ project01.cpp
@@ -9,23 +9,10 @@ int  CountType2Neighbors(int grid[][CMAX], int row, int col);
 void ParseRequirementsString(string requirements, int reqs[]);
 
 void OpenInputFile(string filename, ifstream& inFile) {
-
-	//WORKS
-
-
-	//OpenInputFile -- Open file whose name is stored in filename
-
 	inFile.open(filename.c_str(), std::ifstream::in);
 }
 
 void LoadConstraints(ifstream& inFile, int& num, string& bstring, string& sstring) {
-
-
-	//WORKS
-
-
-	//LoadConstraints -- loads only simulation constraints from inFile after skipping header comment.
-	//Constraints input in order num, birth string, survival string
 	string line;
 	getline(inFile, line); //Pull first comment line
 	getline(inFile, line); //Pull iterations
@@ -39,12 +26,9 @@ void LoadConstraints(ifstream& inFile, int& num, string& bstring, string& sstrin
 
 void LoadGrid(ifstream& inFile, int grid[][CMAX]) {
 	//LoadGrid -- Loads the cell grid from inFile
-
 	string line;
-	//for(int z = 0; z < 9; z++){
 	getline(inFile, line);
-	//}
-	for (int g = 0; g < CMAX; g++) {
+		for (int g = 0; g < CMAX; g++) {
 		for (int h = 0; h < RMAX; h++) {
 			getline(inFile, line, ' ');
 			grid[g][h] = atoi(line.c_str()); //pull values of grid in text file here
@@ -53,362 +37,33 @@ void LoadGrid(ifstream& inFile, int grid[][CMAX]) {
 }
 
 void ComputeNextGrid(int current[][CMAX], int next[][CMAX], int birth[], int survival[]) {
-	//ComputeNextGrid -- uses  current generation to compute next generation using constraints specified in birth and survival arrays
-	/*int neigh1 = 0;
-	int neigh2 = 0;
-	int maxBirth = 0;
-	int minBirth = 9;
-	int maxSurv = 0;
-	int minSurv = 9;
-
-	for (int t = 1; t < 9; t++) {
-	if (minBirth > birth[t] && birth[t] != 0) {
-	minBirth = birth[t];
-	}
-	else if (maxBirth < birth[t]) {
-	maxBirth = birth[t];
-	}
-	}
-
-	for (int r = 1; r < 9; r++) {
-	if (minSurv > survival[r] && survival[r] != 0) {
-	minSurv = survival[r];
-	}
-	else if (maxSurv < survival[r]) {
-	maxSurv = survival[r];
-	}
-	}
-
-
-	for (int i = 0; i < CMAX; i++) {
-	for (int j = 0; j < RMAX; j++) {
-	neigh1 = CountType1Neighbors(current, j, i);
-	neigh2 = CountType2Neighbors(current, j, i);
-
-
-	if (current[i][j] == 1) {
-	if (neigh1 >= minSurv && neigh1 <= maxSurv) {
-	next[i][j] = 1;
-	}
-	else {
-	next[i][j] = 0;
-	}
-	}
-
-	if (current[i][j] == 2) {
-	if (neigh2 >= minSurv && neigh2 <= maxSurv) {
-	next[i][j] = 2;
-	}
-	else {
-	next[i][j] = 0;
-	}
-	}
-
-	if (current[i][j] == 0) {
-	if (neigh1 > neigh2) {
-	if (neigh1 >= minBirth && neigh1 <= maxBirth) {
-	next[i][j] = 1;
-	}
-	else {
-	next[i][j] = 0;
-	}
-	if (neigh1 < neigh2) {
-	if (neigh2 >= minBirth && neigh2 <= maxBirth) {
-	next[i][j] = 2;
-	}
-	else {
-	next[i][j] = 0;
-	}
-	}
-
-	}
-	}
-	if (current[i][j] != 1 && current[i][j] != 2 && current[i][j] != 0) {
-	next[i][j] = 0;
-	}
-	next[i][j];
-	}
-	}*/
-
-
+	int count;
 	for (int i = 0; i < CMAX; i++) {
 		for (int j = 0; j < RMAX; j++) {
 			int neigh1 = CountType1Neighbors(current, i, j);
 			int neigh2 = CountType2Neighbors(current, i, j);
 
 			if (current[i][j] == 1) {
-				switch (neigh1) {
-				case 1:
-					if (survival[1] == 1) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 2:
-					if (survival[2] == 2) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 3:
-					if (survival[3] == 3) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 4:
-					if (survival[4] == 4) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-				case 5:
-					if (survival[5] == 5) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 6:
-					if (survival[6] == 6) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 7:
-					if (survival[7] == 7) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 8:
-					if (survival[8] == 8) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				default:
-					next[i][j] = 0;
+				if (survival[neigh1] == 1) next[i][j] = 1;
+				else {
+					next[i][j] = 0;					
 				}
 			}
 			else if (current[i][j] == 2) {
-				switch (neigh2) {
-				case 1:
-					if (survival[1] == 1) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 2:
-					if (survival[2] == 2) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 3:
-					if (survival[3] == 3) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 4:
-					if (survival[4] == 4) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-				case 5:
-					if (survival[5] == 5) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 6:
-					if (survival[6] == 6) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 7:
-					if (survival[7] == 7) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 8:
-					if (survival[8] == 8) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				default:
+				if (survival[neigh2] == 1) next[i][j] = 2;
+				else {
 					next[i][j] = 0;
 				}
 			}
 			else if (neigh1 > neigh2) {
-				switch (neigh1) {
-				case 1:
-					if (birth[1] == 1) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 2:
-					if (birth[2] == 2) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 3:
-					if (birth[3] == 3) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 4:
-					if (birth[4] == 4) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-				case 5:
-					if (birth[5] == 5) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 6:
-					if (birth[6] == 6) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 7:
-					if (birth[7] == 7) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 8:
-					if (birth[8] == 8) {
-						next[i][j] = 1;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				default:
+				if (birth[neigh1] == 1) next[i][j] = 1;
+				else {
 					next[i][j] = 0;
 				}
 			}
 			else if (neigh1 < neigh2) {
-				switch (neigh2) {
-				case 1:
-					if (birth[1] == 1) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 2:
-					if (birth[2] == 2) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 3:
-					if (birth[3] == 3) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 4:
-					if (birth[4] == 4) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-				case 5:
-					if (birth[5] == 5) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 6:
-					if (birth[6] == 6) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 7:
-					if (birth[7] == 7) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				case 8:
-					if (birth[8] == 8) {
-						next[i][j] = 2;
-					}
-					else {
-						next[i][j] = 0;
-					}
-					break;
-				default:
+				if (birth[neigh1] == 1) next[i][j] = 2;
+				else {
 					next[i][j] = 0;
 				}
 			}
@@ -419,40 +74,9 @@ void ComputeNextGrid(int current[][CMAX], int next[][CMAX], int birth[], int sur
 
 		}
 	}
-
-	/*for (int i = 0; i < CMAX; i++) {
-	for (int j = 0; j < RMAX; j++) {
-	int neigh1 = CountType1Neighbors(current, i, j);
-	int neigh2 = CountType2Neighbors(current, i, j);
-
-	switch (current[i][j]) {
-	case 0:
-
-	break;
-	case 1:
-
-	break;
-	case 2:
-
-	break;
-	default:
-	next[i][j] = 0;
-	break;
-	}
-
-	}
-	}*/
 }
 
 void CopyGrid(const int source[][CMAX], int destination[][CMAX]) {
-
-
-	//NEEDS TO BE TESTED
-
-
-
-	//CopyGrid -- copies contents of source array into destination array
-
 	for (int i = 0; i < CMAX; i++) {
 		for (int j = 0; j < RMAX; j++) {
 			destination[i][j] = source[i][j];
@@ -462,13 +86,6 @@ void CopyGrid(const int source[][CMAX], int destination[][CMAX]) {
 }
 
 int CountType2Neighbors(int grid[][CMAX], int row, int col) {
-
-
-	//WORKS
-
-
-	//CountType1Neighbors -- counts the total number of LIVING Type1 neighbors for the cell at the
-	//grid position specified by row and col.
 	int Total2Neighbors = 0;
 	for (int ir = -1; ir <= 1; ir++) {
 		for (int ic = -1; ic <= 1; ic++) {
@@ -480,22 +97,10 @@ int CountType2Neighbors(int grid[][CMAX], int row, int col) {
 			}
 		}
 	}
-
-
-
-
 	return Total2Neighbors;
 }
 
 int CountType1Neighbors(int grid[][CMAX], int row, int col) {
-
-
-	//WORKS
-
-
-
-	//CountType1Neighbors -- counts the total number of LIVING Type2 neighbors for the cell at the
-	//grid position specified by row and col.
 	int Total1Neighbors = 0;
 	for (int ir = -1; ir <= 1; ir++) {
 		for (int ic = -1; ic <= 1; ic++) {
@@ -507,19 +112,10 @@ int CountType1Neighbors(int grid[][CMAX], int row, int col) {
 			}
 		}
 	}
-
-
-
-
 	return Total1Neighbors;
 }
 
 void ParseRequirementsString(string requirements, int reqs[]) {
-
-
-	//WORKS
-
-
 	const char *buffer = requirements.c_str();
 	char c;
 	while (c = *buffer++)
@@ -553,11 +149,3 @@ void ParseRequirementsString(string requirements, int reqs[]) {
 		}
 	}
 }
-//ParseRequirementsString -- takes a birth or survival string and converts it to integer array
-//assumes that the B or S appears as the first character.Order of constraints does not matter.
-//For example, B13 should produce same result as B31
-//
-//(1's in both reqs[1] and reqs[3], zeros elsewhere)
-//
-//Hint: use string functions to help you scan through each character in the requirements string one
-//at a time and use nested IF or SWITCH to implement logic.
